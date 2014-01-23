8a281f4ac1b41dac6f3ffa85221d418628beedb0
st13902 <Cui_Zuoli@nhn.com>
2014-01-23 14:02:50 +0800

    公共服务 http://open.weibo.com/wiki/%E5%BE%AE%E5%8D%9AAPI

---
+++ src/main/java/com/weibo/api/Common.java
@@ -0,0 +1,80 @@
+/*
+ * @(#)Common.java $version 2014年1月23日
+ *
+ * Copyright 2014 NHN China. All rights Reserved.
+ * NHN China PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.weibo.api;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import com.weibo.http.client.WeiboHttpClient;
+
+/**
+ * nhn weibo-api
+ * com.weibo.api.Common.java
+ * @author CN40101
+ * @date 2014年1月23日
+ */
+@Component
+public class Common {
+
+	private static final String COMMON_GET_COUNTRY_URL = "https://api.weibo.com/2/common/get_country.json";
+	private static final String COMMON_GET_PROVINCE_URL = "https://api.weibo.com/2/common/get_province.json";
+	private static final String COMMON_GET_CITY_URL = "https://api.weibo.com/2/common/get_city.json";
+
+	@Resource
+	private WeiboHttpClient weiboHttpClient;
+
+	/**
+	 * http://open.weibo.com/wiki/2/common/get_country
+	 * @param accessToken
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, String>[] getCountry(String accessToken) {
+		String url = new StringBuffer()
+			.append(COMMON_GET_COUNTRY_URL)
+			.append("?access_token=").append(accessToken)
+			.toString();
+		return weiboHttpClient.get(url, Map[].class);
+	}
+
+	/**
+	 * http://open.weibo.com/wiki/2/common/get_province
+	 * @param country - 国家的国家代码。
+	 * @param accessToken
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, String>[] getProvince(String country, String accessToken) {
+		String url = new StringBuffer()
+			.append(COMMON_GET_PROVINCE_URL)
+			.append("?country=").append(country)
+			.append("&access_token=").append(accessToken)
+			.toString();
+		return weiboHttpClient.get(url, Map[].class);
+	}
+
+	/**
+	 * http://open.weibo.com/wiki/2/common/get_city
+	 * @param province
+	 * @param accessToken
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Map<String, String>[] getCity(String province, String accessToken) {
+		String url = new StringBuffer()
+			.append(COMMON_GET_CITY_URL)
+			.append("?province=").append(province)
+			.append("&access_token=").append(accessToken)
+			.toString();
+		return weiboHttpClient.get(url, Map[].class);
+	}
+
+}
---
+++ src/test/java/com/weibo/api/CommonTest.java
@@ -0,0 +1,61 @@
+/*
+ * @(#)CommonTest.java $version 2014年1月23日
+ *
+ * Copyright 2014 NHN China. All rights Reserved.
+ * NHN China PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.weibo.api;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+
+import com.weibo.api.test.AbstractTest;
+
+/**
+ * nhn weibo-api
+ * com.weibo.api.CommonTest.java
+ * @author CN40101
+ * @date 2014年1月23日
+ */
+@Slf4j
+public class CommonTest extends AbstractTest {
+
+	@Resource
+	private Map<String, String> dataMap;
+
+	@Resource
+	private Common common;
+
+	@Test
+	public void getCountry() {
+		Map<String, String>[] countries = common.getCountry(dataMap.get("accessToken"));
+		for (Map<String, String> country : countries) {
+			log.info(country.toString());
+		}
+	}
+
+	@Test
+	public void getProvince() {
+		String country = "001";
+		Map<String, String>[] provinces = common.getProvince(country, dataMap.get("accessToken"));
+		for (Map<String, String> province : provinces) {
+			log.info(province.toString());
+		}
+	}
+
+	@Test
+	public void getCity() {
+		String province = "001011";
+		Map<String, String>[] cities = common.getCity(province, dataMap.get("accessToken"));
+		for (Map<String, String> city : cities) {
+			log.info(city.toString());
+		}
+	}
+
+}
